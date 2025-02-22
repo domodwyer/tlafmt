e45de40ff1f59818a296b46cb5ebae11a84adf04
Dom Dwyer <dom@itsallbroken.com>
2025-02-22 21:42:38 +0100

    feat(cli): nicer CLI with help text
    Implement a proper CLI interface.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
-use std::env::args;
+use std::path::PathBuf;
 
+use clap::Parser;
 use libtlafmt::ParsedFile;
+use thiserror::Error;
 
-fn main() {
-    let input = std::fs::read_to_string(args().nth(1).expect("no filename passed as argument"))
-        .expect("must read file path");
+/// Formatter of TLA+ specs.
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Path to the TLA+ file to format.
+    #[arg()]
+    file: PathBuf,
+}
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("failed to read input file: {0}")]
+    ReadFile(std::io::Error),
+
+    #[error("formatting error: {0}")]
+    Format(#[from] libtlafmt::Error),
+}
+
+fn main() -> Result<(), Error> {
+    let args = Args::parse();
+    let input = std::fs::read_to_string(args.file).map_err(Error::ReadFile)?;
 
     ParsedFile::new(input.as_str())
         .unwrap()
         .format(std::io::stdout().lock())
         .expect("format error");
+
+    Ok(())
 }

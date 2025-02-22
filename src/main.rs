67fd8f34dded4b7cb0f182f8e726d98386b540f3
Dom Dwyer <dom@itsallbroken.com>
2025-02-22 22:15:54 +0100

    feat(cli): add --check mode
    Validate an input file is formatted, returning an error (exit code 3) if
    not.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ struct Args {
     /// Path to the TLA+ file to format.
     #[arg()]
     file: PathBuf,
+
+    /// Check the input file and exit with an error (code 3) if it needs
+    /// formatting.
+    #[arg(long)]
+    check: bool,
 }
 
 #[derive(Debug, Error)]
@@ -24,12 +29,29 @@ enum Error {
 
 fn main() -> Result<(), Error> {
     let args = Args::parse();
-    let input = std::fs::read_to_string(args.file).map_err(Error::ReadFile)?;
 
-    ParsedFile::new(input.as_str())
-        .unwrap()
-        .format(std::io::stdout().lock())
-        .expect("format error");
+    let input = std::fs::read_to_string(args.file).map_err(Error::ReadFile)?;
+    let parsed = ParsedFile::new(input.as_str())?;
+
+    if args.check {
+        return check(&input, parsed);
+    }
+
+    parsed.format(std::io::stdout().lock())?;
+    Ok(())
+}
+
+fn check(input: &str, parsed: ParsedFile<'_>) -> Result<(), Error> {
+    // Allocate a buffer to render the normalised spec into, which will be
+    // approximately the same length as the input text.
+    let mut buf = Vec::with_capacity(input.len());
+
+    parsed.format(&mut buf)?;
+
+    if buf.trim_ascii() != input.trim_ascii().as_bytes() {
+        eprintln!("input file needs formatting");
+        std::process::exit(3);
+    }
 
     Ok(())
 }

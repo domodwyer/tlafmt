1429fc48109bfd4733d669bcff3052e7f151ab18
Dom Dwyer <dom@itsallbroken.com>
2025-02-22 23:08:58 +0100

    feat(cli): --in-place formatting of input file
    Support writing the formatted output into the source file.
    
    This --in-place overwrite is indirected through a tempfile to ensure
    atomicity; overwriting the input file with a half-rendered output spec
    (due to error or a fast Ctrl+C) would be an annoying UX.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,11 @@ struct Args {
     /// formatting.
     #[arg(long)]
     check: bool,
+
+    /// Overwrite the source file with the formatted output instead of printing
+    /// it to stdout.
+    #[arg(short, long, conflicts_with = "check")]
+    in_place: bool,
 }
 
 #[derive(Debug, Error)]
@@ -25,19 +30,53 @@ enum Error {
 
     #[error("formatting error: {0}")]
     Format(#[from] libtlafmt::Error),
+
+    /// Creating a temporary file for --in-place output.
+    #[error("failed to create temporary file in current dir: {0}")]
+    CreateTempFile(std::io::Error),
+
+    /// Persisting the formatted output for --in-place.
+    #[error("failed to persist formatted output: {0}")]
+    SaveTempFile(std::io::Error),
 }
 
 fn main() -> Result<(), Error> {
     let args = Args::parse();
 
-    let input = std::fs::read_to_string(args.file).map_err(Error::ReadFile)?;
+    let input = std::fs::read_to_string(&args.file).map_err(Error::ReadFile)?;
     let parsed = ParsedFile::new(input.as_str())?;
 
     if args.check {
+        assert!(!args.in_place);
         return check(&input, parsed);
     }
 
+    if args.in_place {
+        assert!(!args.check);
+        return in_place(args, &parsed);
+    }
+
     parsed.format(std::io::stdout().lock())?;
+    Ok(())
+}
+
+fn in_place(args: Args, parsed: &ParsedFile<'_>) -> Result<(), Error> {
+    // For in-place output, first render to a temporary file and then move it to
+    // the input path (somewhat) atomically to prevent a ctrl+c or crash during
+    // execution from causing the input file to be only half populated.
+    let mut file = tempfile::Builder::new()
+        .prefix(".tlafmt")
+        // 6 bytes of randomness here
+        .suffix(".rs")
+        // Tempfiles across filesystems can be problematic, so use ./
+        .tempfile_in("./")
+        .map_err(Error::CreateTempFile)?;
+
+    parsed.format(&mut file)?;
+
+    file.persist(args.file)
+        .map_err(|v| Error::SaveTempFile(v.error))?;
+
     Ok(())
 }
 
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,8 +1,28 @@
+use std::path::PathBuf;
+
 use assert_cmd::Command;
 use predicates::prelude::*;
+use tempfile::TempDir;
+
+/// Path to an unformatted file.
+const BAD_PATH: &str = "libtlafmt/tests/corpus/differential_equations.tla";
+
+/// Path to a formatted file.
+///
+/// NOTE: this file includes a YAML header that does not appear in the input
+/// file.
+const GOOD_PATH: &str =
+    "libtlafmt/src/snapshots/libtlafmt__tests__corpus@differential_equations.tla.snap";
 
 fn cmd() -> Command {
     Command::cargo_bin("tlafmt").unwrap()
+}
+
+fn dir() -> TempDir {
+    tempfile::Builder::new()
+        .prefix(".tlafmt-test")
+        .tempdir_in("./")
+        .expect("cannot create tempdir for test state")
 }
 
 /// This test asserts what is part of the CLI and the documentation for it.
@@ -33,9 +53,10 @@ Arguments:
   <FILE>  Path to the TLA+ file to format
 
 Options:
-      --check    Check the input file and exit with an error (code 3) if it needs formatting
-  -h, --help     Print help
-  -V, --version  Print version
+      --check     Check the input file and exit with an error (code 3) if it needs formatting
+  -i, --in-place  Overwrite the source file with the formatted output instead of printing it to stdout
+  -h, --help      Print help
+  -V, --version   Print version
 "
     );
 }
@@ -46,7 +67,7 @@ fn test_check_mode() {
     // Failure case from test corpus
     cmd()
         .arg("--check")
-        .arg("libtlafmt/tests/corpus/differential_equations.tla")
+        .arg(BAD_PATH)
         .assert()
         .failure()
         .stdout(predicate::eq(""))
@@ -56,10 +77,47 @@ fn test_check_mode() {
     // Success from corpus snapshot test output.
     cmd()
         .arg("--check")
-        .arg("libtlafmt/src/snapshots/libtlafmt__tests__corpus@differential_equations.tla.snap")
+        .arg(GOOD_PATH)
         .assert()
         .success()
         .stdout(predicate::eq(""))
         .stderr(predicate::eq(""))
         .code(predicate::eq(0));
+}
+
+#[test]
+fn test_in_place() {
+    let wd = dir();
+
+    let mut file = PathBuf::from(wd.path());
+    file.push("test.rs");
+
+    std::fs::copy(BAD_PATH, &file).expect("cannot copy file for test");
+
+    // Run the formatter with --in-place
+    cmd()
+        .arg("--in-place")
+        .arg(file.to_str().unwrap())
+        .assert()
+        .success()
+        .stdout(predicate::eq(""))
+        .stderr(predicate::eq(""))
+        .code(predicate::eq(0));
+
+    // Run the formatter without --in-place to obtain the control output.
+    let control = String::from_utf8(
+        cmd()
+            .arg(BAD_PATH)
+            .assert()
+            .success()
+            .get_output()
+            .stdout
+            .clone(),
+    )
+    .unwrap();
+
+    let got = std::fs::read_to_string(file).unwrap();
+
+    // Confirm the file matches the formatted sample file.
+    pretty_assertions::assert_eq!(control, got);
 }

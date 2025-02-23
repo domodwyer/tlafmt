e179787fc3f1cb8578b49bc2f7c751b22b2badc7
Dom Dwyer <dom@itsallbroken.com>
2025-02-23 18:40:33 +0100

    fix: inline list comment indentation
    Comments require special lowering treatment to ensure they're indented
    correctly, as they're attributed to being a child of a list node in the
    AST when they're not actually part of the list in the source code, but
    rather the following block.
    
    Previously this used a heuristic to locate these misattributed comments
    and correct them, but it didn't correctly account for comments that
    appear within the middle of a set of nested list (specifically, at the
    end of a list nested within an outer list).
    
    This commit unifies comment handling using a simpler method: if the
    comment wasn't indented in source, don't indent it now.

--- libtlafmt/src/ast_format/comment.rs
+++ libtlafmt/src/ast_format/comment.rs
@@ -13,51 +13,20 @@ pub(super) fn format_comment<'a, W>(
 where
     W: std::io::Write,
 {
-    let orig = writer.indent_get();
-
     // Block comments should be rendered "as-is" without additional indentation.
-    if def.kind() == "block_comment" {
-        writer.indent_set(0);
-        writer.push(Token::Comment(get_str(&def, input)))?;
-        writer.indent_set(orig);
-        return Ok(());
-    }
-
-    // A fix for comments adjacent to the next node appearing at the tail of a
-    // list node.
-    let in_list = def
-        .parent()
-        .is_some_and(|v| v.kind() == "disj_list" || v.kind() == "conj_list");
-
-    // Rewriting only occurs for disjunction / conjunction lists.
-    if !in_list {
+    //
+    // If a comment was not indented, it should be rendered without
+    // formatter-added indentation below this branch.
+    if def.kind() != "block_comment" && def.start_position().column != 0 {
         writer.push(Token::Comment(get_str(&def, input)))?;
         return Ok(());
     }
 
-    // And if the comment appears at the end of the list.
-    if !is_comments_next(def) {
-        writer.push(Token::Comment(get_str(&def, input)))?;
-        return Ok(());
-    }
+    let orig = writer.indent_get();
 
     writer.indent_set(0);
     let ret = writer.push(Token::Comment(get_str(&def, input)));
     writer.indent_set(orig);
 
     ret.map_err(Into::into)
-}
-
-/// Return `true` if the next sibling nodes of `ptr` are exclusively comments,
-/// or no siblings exist.
-fn is_comments_next(mut ptr: Node<'_>) -> bool {
-    while let Some(n) = ptr.next_sibling() {
-        if n.kind() != "comment" {
-            return false;
-        }
-
-        ptr = n;
-    }
-
-    true
 }

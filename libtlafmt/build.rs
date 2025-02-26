0a3ee2b48b9085bb3274a3b1009a5bc035f8e50a
Dom Dwyer <dom@itsallbroken.com>
2025-02-26 20:26:57 +0100

    ci: fix missing_docs lint
    It was in the "clippy" block, but this is a rustc lint.

--- libtlafmt/build.rs
+++ libtlafmt/build.rs
@@ -1,4 +1,6 @@
-pub fn main() {
+#![allow(missing_docs)]
+
+fn main() {
     // This statement prevents cargo from complaining about an unknown (to it)
     // cfg for fuzzing.
     println!("cargo::rustc-check-cfg=cfg(fuzzing)");
